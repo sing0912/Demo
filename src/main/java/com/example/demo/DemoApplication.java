7d2ef85d2891664ff050eeb7bdeac878cb0ffd88
sing0912 <hidragon79@gmail.com>
2023-02-15 23:46:23 +0900

    testok

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    // test good
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
